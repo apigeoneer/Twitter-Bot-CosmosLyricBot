64ef2cbd2534d9275c6ba6c8a46d58def7f1279a
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-02-07 11:45:23 +0530

    ver-1 : not working,  TypeError: Cannot read property 'id_str' of undefined

--- bot3.js
+++ bot3.js
@@ -1,45 +1,47 @@
 console.log('bot3 starting!')
 /**
- * Retweets the tweets that have a hashtag
+ * Retweets the tweets that w/ a particular hashtag
  */
 
-let Twit = require('twit')
-let config = require('./config')
-let T = new Twit(config)
+const Twit = require('twit')
+const config = require('./config')
+const T = new Twit(config)
 
 // // Setting up a user stream
-// let stream = T.stream('user')
+// let stream = T.stream('user') *DEPRICATED, *NOT_REQUIRED
 
-// Search for the tweets w/ #CosmosLyricBot
- let params = {
-    q: '#CosmosLyricBot OR #CosmosLyricsBot #cosmoslyricbot OR #cosmoslyricsbot OR #COSMOSLYRICBOT OR #COSMOSLYRICSBOT OR #Cosmoslyricbot OR #Cosmoslyricsbot'
-        , result_type: 'recent'
-};
 
-function retweetHashtag() {
+
+
+const retweet = () => {
+    // Search for the tweets w/ your #
+    let params = {
+        q: '#CosmosLyrics',
+        result_type: 'recent',
+        lang: 'en'
+    };
 
     T.get('search/tweets', params, (err, data) => {
-        // log out any errors & responses
-        console.log(error, data)
-        if (!err) {
-            // grab the id of the tweet to be retweeted
-            let retweetId = data.statuses[0].id_str;
-            // tell Twitter we want to retweet the tweet if retweetId
-            T.post('statuses/retweet/' + retweetId, {}, (error, response) => {
-                if(response) {
-                    console.log("Retweeted a tweet w/ #CosmosArtBot!");
-                } else if(error) {
-                    console.log("Failed to retweet a tweet w/ #CosmosArtBot!");
-                }
-            });
-        } else {
-            console.log("Error w/ hashtag serach! Failed to retweet!", error);
+        console.log('Searching #s...');
+        
+        // grab the id of the tweet to be retweeted
+        let retweetId = data.statuses[0].id_str;
+
+        if (err) {
+            console.log("Error w/ # serach! Failed to retweet!", err);
         }
+
+        // tell Twitter we want to retweet the tweet if retweetId
+        T.post('statuses/retweet/:id', { id: retweetId }, (err, response) => {
+            if(err) {
+                console.log("Failed to retweet a tweet w/ your # !");
+            }
+            console.log("Retweeted a tweet w/ your # !"); 
+        });
     });
+};
 
-    // try to retweet as soon as the program is run
-    retweetHashtag();
-    // & every 15 mins
-    setInterval(retweetHashtag, 1000*60*15);
-}
-
+// try to retweet as soon as the program is run
+retweet();
+// & every 1 min
+setInterval(retweet, 1000*60);
