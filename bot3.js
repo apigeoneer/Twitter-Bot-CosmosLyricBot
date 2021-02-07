fbf436714a355523232fd737721b5560ab4c8b3d
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-02-07 10:45:16 +0530

    retweet every tweet w/ a particular hashtag

--- bot3.js
+++ bot3.js
@@ -7,8 +7,39 @@ let Twit = require('twit')
 let config = require('./config')
 let T = new Twit(config)
 
-// Setting up a user stream
-let stream = T.stream('user')
+// // Setting up a user stream
+// let stream = T.stream('user')
 
+// Search for the tweets w/ #CosmosLyricBot
+ let params = {
+    q: '#CosmosLyricBot OR #CosmosLyricsBot #cosmoslyricbot OR #cosmoslyricsbot OR #COSMOSLYRICBOT OR #COSMOSLYRICSBOT OR #Cosmoslyricbot OR #Cosmoslyricsbot'
+        , result_type: 'recent'
+};
 
+function retweetHashtag() {
+
+    T.get('search/tweets', params, (err, data) => {
+        // log out any errors & responses
+        console.log(error, data)
+        if (!err) {
+            // grab the id of the tweet to be retweeted
+            let retweetId = data.statuses[0].id_str;
+            // tell Twitter we want to retweet the tweet if retweetId
+            T.post('statuses/retweet/' + retweetId, {}, (error, response) => {
+                if(response) {
+                    console.log("Retweeted a tweet w/ #CosmosArtBot!");
+                } else if(error) {
+                    console.log("Failed to retweet a tweet w/ #CosmosArtBot!");
+                }
+            });
+        } else {
+            console.log("Error w/ hashtag serach! Failed to retweet!", error);
+        }
+    });
+
+    // try to retweet as soon as the program is run
+    retweetHashtag();
+    // & every 15 mins
+    setInterval(retweetHashtag, 1000*60*15);
+}
 
